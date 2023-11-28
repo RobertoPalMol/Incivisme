0e20307e97acfe0704ad632f07433f224a73fdd9
robpalmol <robpalmol@alu.edu.gva.es>
2023-11-28 12:00:36 +0100

    commit 28 nov

--- app/src/main/java/com/example/incivisme/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/incivisme/ui/home/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.incivisme.ui.home;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.incivisme.R;
 import com.example.incivisme.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -23,10 +26,23 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button localizacion = view.findViewById(R.id.button_location);
+
+        localizacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
         final TextView textView = binding.buttonLocation;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
     }
 
     @Override
