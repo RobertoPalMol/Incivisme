65f1ed2a4d4bbc149c2e827b4ff3e024b9bcbf37
robpalmol <robpalmol@alu.edu.gva.es>
2023-12-01 10:23:16 +0100

    commit 1 dic

--- app/src/main/java/com/example/incivisme/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/incivisme/ui/home/HomeFragment.java
@@ -2,8 +2,11 @@ package com.example.incivisme.ui.home;
 
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +17,27 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.incivisme.R;
 import com.example.incivisme.databinding.FragmentHomeBinding;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.internal.ApiKey;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LastLocationRequest;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.Executor;
 
 public class HomeFragment extends Fragment {
 
@@ -27,6 +45,110 @@ public class HomeFragment extends Fragment {
 
     private ActivityResultLauncher<String[]> locationPermissionRequest;
 
+    Location mLastLocation = new Location("LastLocation");
+    FusedLocationProviderClient mFusedLocationClient = new FusedLocationProviderClient() {
+        @NonNull
+        @Override
+        public Task<Void> flushLocations() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Location> getCurrentLocation(int i, @Nullable CancellationToken cancellationToken) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Location> getCurrentLocation(@NonNull CurrentLocationRequest currentLocationRequest, @Nullable CancellationToken cancellationToken) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Location> getLastLocation() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Location> getLastLocation(@NonNull LastLocationRequest lastLocationRequest) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<LocationAvailability> getLocationAvailability() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> removeLocationUpdates(@NonNull PendingIntent pendingIntent) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> removeLocationUpdates(@NonNull LocationCallback locationCallback) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> removeLocationUpdates(@NonNull LocationListener locationListener) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> requestLocationUpdates(@NonNull LocationRequest locationRequest, @NonNull PendingIntent pendingIntent) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> requestLocationUpdates(@NonNull LocationRequest locationRequest, @NonNull LocationCallback locationCallback, @Nullable Looper looper) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> requestLocationUpdates(@NonNull LocationRequest locationRequest, @NonNull LocationListener locationListener, @Nullable Looper looper) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> requestLocationUpdates(@NonNull LocationRequest locationRequest, @NonNull Executor executor, @NonNull LocationCallback locationCallback) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> requestLocationUpdates(@NonNull LocationRequest locationRequest, @NonNull Executor executor, @NonNull LocationListener locationListener) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> setMockLocation(@NonNull Location location) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Task<Void> setMockMode(boolean b) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public ApiKey<Api.ApiOptions.NoOptions> getApiKey() {
+            return null;
+        }
+    };
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
@@ -53,7 +175,7 @@ public class HomeFragment extends Fragment {
                     }
                 }
         );
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
 
         localizacion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +202,19 @@ public class HomeFragment extends Fragment {
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION
             });
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    location -> {
+                        if (location != null) {
+                            mLastLocation = location;
+                            binding.localitzacio.setText(
+                                    String.format("Latitud: %1$.4f \n Longitud: %2$.4f\n Hora: %3$tr",
+                                            mLastLocation.getLatitude(),
+                                            mLastLocation.getLongitude(),
+                                            mLastLocation.getTime()));
+                        } else {
+                            binding.localitzacio.setText("Sense localització coneguda");
+                        }
+                    });
         } else {
             Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
         }
